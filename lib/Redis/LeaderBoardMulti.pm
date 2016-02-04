519619a0f943f9038fc8fb4215d99273cf9097ef
Ichinose Shogo <shogo82148@gmail.com>
2016-02-04 11:45:25 +0900

    init commit

---
+++ lib/Redis/LeaderBoardMulti.pm
@@ -0,0 +1,39 @@
+package Redis::LeaderBoardMulti;
+use 5.008005;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Redis::LeaderBoardMulti - It's new $module
+
+=head1 SYNOPSIS
+
+    use Redis::LeaderBoardMulti;
+
+=head1 DESCRIPTION
+
+Redis::LeaderBoardMulti is ...
+
+=head1 LICENSE
+
+Copyright (C) Ichinose Shogo.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Ichinose Shogo E<lt>shogo82148@gmail.comE<gt>
+
+=cut
+
