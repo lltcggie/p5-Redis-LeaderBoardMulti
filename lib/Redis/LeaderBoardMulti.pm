a6469d341cf5c7b6008987b2a7ba8d0edb976b07
Ichinose Shogo <shogo82148@gmail.com>
2016-12-24 09:07:05 +0900

    Checking in changes prior to tagging of version 0.02.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 9c4e98c..a73711d 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,9 @@ Revision history for Perl extension Redis-LeaderBoardMulti
    
     {{$NEXT}}
    
    +0.02 2016-12-24T00:03:39Z
    +    - fix errors in 32bit environment
    +
     0.01 2016-03-18T16:55:12Z
    
         - original version

--- lib/Redis/LeaderBoardMulti.pm
+++ lib/Redis/LeaderBoardMulti.pm
@@ -3,7 +3,7 @@ use 5.010;
 use strict;
 use warnings;
 
-our $VERSION = "0.01";
+our $VERSION = "0.02";
 
 use Redis::LeaderBoardMulti::Member;
 use Redis::Transaction qw/multi_exec watch_multi_exec/;
